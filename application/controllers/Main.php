677d8e91d647f2e165f0d1cb759e660627cb6011
kapil707 <kapil707sharma@gmail.com>
2023-01-13 14:30:11 +0530

    rep-2023-01-13-1430

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -53,48 +53,36 @@ class Main extends CI_Controller {
 		$title0 = "Our top brands";
 		$data["title0"] = $title0;
 		//$this->Chemist_Model->featured_brand_json_new();
-		echo $featured_brand_json_new = file_get_contents('./json_api/featured_brand_json_new.json');
-		$result0 = $featured_brand_json_new;
+		$result0 = file_get_contents('./json_api/featured_brand_json_new.json');
 		$result0 = json_decode("[$result0]", true);	
 		$data["result0"] = $result0;
-		
 
-
-		if ($_SESSION["result1"] == "") {
-			$_SESSION["result1"] = $this->Chemist_Model->new_medicine_this_month_json_new();
-		}
-		$result1 = $_SESSION["result1"];
+		/***************************************************/
+		$result1 = file_get_contents('./json_api/new_medicine_this_month_json_new.json');
 		$result1 = json_decode("[$result1]", true);	
 		$data["result1"] = $result1;
 
-		if ($_SESSION["result2"] == "") {
-			$_SESSION["result2"] = $this->Chemist_Model->hot_selling_today_json_new();
-		}
-		$result2 = $_SESSION["result2"];
+		/***************************************************/
+		$result2 = file_get_contents('./json_api/hot_selling_today_json_new.json');
 		$result2 = json_decode("[$result2]", true);
 		$data["result2"] = $result2;
 
-		if ($_SESSION["result3"] == "") {
-			$_SESSION["result3"] = $this->Chemist_Model->must_buy_medicines_json_new();
-		}
-		$result3 = $_SESSION["result3"];
+		/***************************************************/
+		$result3 = file_get_contents('./json_api/must_buy_medicines_json_new.json');
 		$result3 = json_decode("[$result3]", true);
 		$data["result3"] = $result3;
 
-		if ($_SESSION["result4"] == "") {
-			$_SESSION["result4"] = $this->Chemist_Model->frequently_use_medicines_json_new();
-		}
-		$result4 = $_SESSION["result4"];
+		/***************************************************/
+		$result4 = file_get_contents('./json_api/frequently_use_medicines_json_new.json');
 		$result4 = json_decode("[$result4]", true);
 		$data["result4"] = $result4;
 		
-		if ($_SESSION["result5"] == "") {
-			$_SESSION["result5"] = $this->Chemist_Model->stock_now_available();
-		}
-		$result5 = $_SESSION["result5"];
+		/***************************************************/
+		$result5  = file_get_contents('./json_api/stock_now_available.json');
 		$result5 = json_decode("[$result5]", true);
 		$data["result5"] = $result5;
 
+		/***************************************************/
 		$data["result6"] = "";
 				
 		$this->load->view('home/home', $data);
