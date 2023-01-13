ac1c18df0990b16145b031e5c21eeb452d19b8f1
kapil707 <kapil707sharma@gmail.com>
2023-01-13 14:09:29 +0530

    rep-2023-01-13-1409

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -131,7 +131,11 @@ class Home extends CI_Controller {
 
 		$title0 = "Our top brands";
 		$data["title0"] = $title0;
-		$result0 = $this->Chemist_Model->featured_brand_json_new();
+		//$this->Chemist_Model->featured_brand_json_new();
+		$featured_brand_json_new = fopen("json_api/featured_brand_json_new.json", "r") or die("Unable to open file!");
+		echo fread($featured_brand_json_new,filesize("webdictionary.txt"));
+		fclose($featured_brand_json_new);
+		$result0 = $featured_brand_json_new;
 		$result0 = json_decode("[$result0]", true);	
 		$data["result0"] = $result0;
 
