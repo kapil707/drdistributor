de9e822ada8f2d910ec3e789e3959689cde0dbc6
kapil707 <kapil707sharma@gmail.com>
2023-01-19 16:14:45 +0530

    rep-2023-01-19-1614

--- application/controllers/exe01/Exe02.php
+++ application/controllers/exe01/Exe02.php
@@ -97,6 +97,7 @@ class Exe02 extends CI_Controller
 
 			$qry .= "update tbl_medicine_image set download_status=1 where id='$row->id';";
 		}
+		
 		if (empty($items)) {
 			$result = $this->db->query("select * from tbl_acm_other where download_status=0 limit 100")->result();
 			foreach ($result as $row) {
@@ -119,7 +120,33 @@ class Exe02 extends CI_Controller
 	
 				$items .= '{"query_type":"acm_other","code":"'.$code.'","status":"'.$status.'","exp_date":"'.$exp_date.'","password":"'.$password.'","broadcast":"'.$broadcast.'","block":"'.$block.'","image":"'.$image.'","user_phone":"'.$user_phone.'","user_email":"'.$user_email.'","user_address":"'.$user_address.'","user_update":"'.$user_update.'","order_limit":"'.$order_limit.'","new_request":"'.$new_request.'","website_limit":"'.$website_limit.'","android_limit":"'.$android_limit.'"},';
 	
-				$qry.= "update tbl_acm_other set download_status=1 where id='$row->id'";
+				$qry.= "update tbl_acm_other set download_status=1 where id='$row->id';";
+			}
+		}
+
+		if (empty($items)) {
+			$result = $this->db->query("select * from tbl_acm_other where download_status=0 limit 100")->result();
+			foreach ($result as $row) {
+
+				$code 			= $row->code;
+				$status 		= $row->status;
+				$exp_date 		= $row->exp_date;
+				$password 		= $row->password;
+				$broadcast 		= $row->broadcast;
+				$block 			= $row->block;
+				$image 			= $row->image;
+				$user_phone 	= $row->user_phone;
+				$user_email 	= $row->user_email;
+				$user_address 	= $row->user_address;
+				$user_update 	= $row->user_update;
+				$order_limit 	= $row->order_limit;
+				$new_request 	= $row->new_request;
+				$website_limit 	= $row->website_limit;
+				$android_limit 	= $row->android_limit;
+	
+				$items .= '{"query_type":"acm_other","code":"'.$code.'","status":"'.$status.'","exp_date":"'.$exp_date.'","password":"'.$password.'","broadcast":"'.$broadcast.'","block":"'.$block.'","image":"'.$image.'","user_phone":"'.$user_phone.'","user_email":"'.$user_email.'","user_address":"'.$user_address.'","user_update":"'.$user_update.'","order_limit":"'.$order_limit.'","new_request":"'.$new_request.'","website_limit":"'.$website_limit.'","android_limit":"'.$android_limit.'"},';
+	
+				$qry.= "update tbl_acm_other set download_status=1 where id='$row->id';";
 			}
 		}
 
@@ -136,7 +163,7 @@ class Exe02 extends CI_Controller
 	
 				$items .= '{"query_type":"low_stock_alert","vdt":"'.$vdt.'","acno":"'.$acno.'","slcd":"'.$slcd.'","itemc":"'.$itemc.'","uid":"'.$uid.'"},';
 	
-				$qry.= "update tbl_low_stock_alert set download_status=1 where id='$row->id'";
+				$qry.= "update tbl_low_stock_alert set download_status=1 where id='$row->id';";
 			}
 		}
 		if (!empty($items)) {
